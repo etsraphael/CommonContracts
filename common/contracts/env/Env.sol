4f01754506b3c79ac88a15ad110849083a096206
etsraphael <ets_raphael@outlook.fr>
2022-12-27 22:25:40 +1100

    applyFeeAndGetAmount added

--- common/contracts/env/Env.sol
+++ common/contracts/env/Env.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: MIT
 
+import "./../utils/math/SafeMath.sol";
+
 pragma solidity ^0.8.0;
 
 abstract contract Env {
     address payable private _protocol;
     uint256 public _fee;
+
+    // ============ Library ============
+
+    using SafeMath for uint256;
 
     // ============ Constructor ============
 
@@ -21,6 +27,21 @@ abstract contract Env {
 
     function fee() public view virtual returns (uint256) {
         return _fee;
+    }
+
+    // send transaction with protocol fees
+    function applyFeeAndGetAmount(address payable to, uint256 value, uint256 owner_fee) public returns (uint256) {
+
+        // send protocol fee
+        uint256 protocol_fee = value.mul(fee()).div(100);
+        payable(protocol()).transfer(protocol_fee);
+
+        // send owner fee
+        uint256 owner_fee_value = value.mul(owner_fee).div(100);
+        to.transfer(owner_fee_value);
+
+        // return amount
+        return value.sub(protocol_fee).sub(owner_fee_value);
     }
 
 }
